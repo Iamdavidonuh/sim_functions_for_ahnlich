e0b462272616cc7e36efc1e4b2d06ce97df3a462
David Onuh <davidonuh1@gmail.com>
2024-05-04 00:48:23 +0100

    Add impl for listed algorithms

--- README.md
+++ README.md
@@ -9,6 +9,51 @@ Below are the algorithms
 
 - Euclidean similarity
 - dot-product
+
 - Cosine similarity, etc
 
 
+
+
+# COSINE SIMILARITY
+Cosine similiarity is the cosine of the angles between vectors.
+It tries to find how close or similar two vector points are.
+It is scalent invarient meaning it is unaffected by the length of the vectors.
+
+
+Cosine of the angles between vectors shows how similar, dissimilar or orthogonal(independent).
+
+
+The range of similarity for cosine similarity ranges from -1 to 1:
+where:
+    1 means similar
+    -1 different.
+    0 means Orthogonal
+
+
+To calculate the cosine similarity for two vectors, we need to:
+- Calculate the dot product of both vectors
+- Find the product of the magnitude of both vectors.
+   A magnitude of a vector can be calculated using pythagoras theorem
+   which is sqrt( A^2 + B^2) where A and B are two vectors.
+
+- divide the dot product by the product of the magnitude of both vectors.
+    ```
+        cos(0) = A.B / ||A||.||B||
+    ```
+
+
+# Euclidean similarity
+ d(p,q)= sqrt { (p-q)^2 }
+Euclidean distance is the square root of the sum of squared differences between corresponding
+elements of the two vectors.
+
+Note that the formula treats the values of X and Y seriously: no adjustment is made for
+differences in scale. Euclidean distance is only appropriate for data measured on the same
+scale(meaning it is  scale varient).
+The distance derived is purely based on the difference between both vectors and is therefore,
+prone to skewness if the units of the vectors are vastly different.
+
+Hence, it is important to ensure that the data is normalised before applying the Euclidean distance function.
+Additionally due to the curse of dimensionality, the effectiveness of Euclidean distance breaks down as 
+dimensionality increases.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,78 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use ndarray::prelude::*;
+
+fn cosine_similarity(
+    first: ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+    second: ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+) -> f64 {
+    // formular = dot product of vectors / product of the magnitude of the vectors
+    // maginiture of a vector can be calcuated using pythagoras theorem.
+    // sqrt of sum of vector values
+    // for magnitutude of A( ||A||) =[1,2]
+    // sqrt[(1)^2 + (2)^2]
+    //
+    //
+
+    let dot_product = dot_product(&first, &second);
+
+    // the magnitude can be calculated using the arr.norm method.
+    //
+
+    let mag_first = &first
+        .iter()
+        .map(|x| x * x)
+        .map(|x| x as f64)
+        .sum::<f64>()
+        .sqrt();
+
+    let mag_second = &second
+        .iter()
+        .map(|x| x * x)
+        .map(|x| x as f64)
+        .sum::<f64>()
+        .sqrt();
+
+    let cosine_sim = dot_product / (mag_first * mag_second);
+
+    cosine_sim
+}
+// for mulitplication of a matrix, the number of columns in the first matrix == the number of rows
+// in the second
+// A.B != B.A
+
+fn dot_product(
+    first: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+    second: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+) -> f64 {
+    // transpose if the shapes are equal that is
+    // check that for a A = m*n and B = p * q
+    // m = q and n = q
+    // else check that it's already transposed
+
+    let dot_product;
+
+    if first.shape() == second.shape() {
+        dot_product = second.dot(&first.t());
+    } else {
+        dot_product = first.dot(second);
+    }
+
+    dot_product
+}
+
+fn euclidean_distance(
+    first: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+    second: &ndarray::ArrayBase<ndarray::OwnedRepr<f64>, Ix1>,
+) -> f64 {
+    // Calculate the sum of squared differences for each dimension
+    let mut sum_of_squared_differences = 0.0;
+    for (&coord1, &coord2) in first.iter().zip(second.iter()) {
+        let diff = coord1 - coord2;
+        sum_of_squared_differences += diff * diff;
+    }
+
+    // Calculate the square root of the sum of squared differences
+    let distance = f64::sqrt(sum_of_squared_differences);
+    distance
 }
 
 #[cfg(test)]
@@ -7,8 +80,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_cosine_similarity_on_same_vectors() {
+        let first_vector = ndarray::Array1::<f64>::zeros(2).map(|x| x + 2.0);
+        let second_vector = ndarray::Array1::<f64>::zeros(2).map(|x| x + 2.0);
+
+        let similarity = cosine_similarity(first_vector, second_vector).round();
+
+        assert_eq!(similarity, 1.0);
     }
 }
