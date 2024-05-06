c4e141fffd184ccbe77e08839b9b820b117bba66
David Onuh <davidonuh1@gmail.com>
2024-05-06 13:50:34 +0100

    moce away from gensim, use huggingface's sentence_transformer

--- sim_py/vectors.py
+++ sim_py/vectors.py
@@ -1,44 +1,26 @@
-from gensim.test.utils import common_texts
-from gensim.models import KeyedVectors, Word2Vec
-import gensim.downloader as api
+from sentence_transformers import SentenceTransformer
 import json
 
-def save_simple_model():
-    model = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
-    model.save("word2vec.model")
-
-
 def get_model(model_name=None):
-
-    print(api.info())
-    corpus = api.load("quora-duplicate-questions", return_path=True)
-
-    corpus = api.load('text8')  # download the corpus and return it opened as an iterable
-    model = Word2Vec(corpus)
-    #model = KeyedVectors.load_word2vec_format(corpus, binary=False)
-    #w = Word2Vec.load_word2vec_format(corpus, binary=True)
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     return model
 
 
 
-
-def get_keyed_vectors(words: list):
-
-    # model = Word2Vec.load("word2vec.model")
+def get_keyed_vectors(sentences: list):
 
     model = get_model()
-
     response = {}
+    for sentence in sentences:
 
-    for word in words:
-        if word in model.wv:
-            response[word] = model.wv[word].tolist()
+        response[sentence] = model.encode(sentence).tolist()
 
     json_response = json.dumps(response, indent=4)
 
     with open("test_json.json", "a+") as f:
         f.write(json_response)
 
+    print(response)
 
     return response
 
